8abef003c3bbb8642e5a3c2ec7559c2808aafef5
deborah <deborahregs@gmail.com>
2021-12-10 15:48:41 -0300

    servicoRepository

---
+++ src/main/java/com/dbc/walletapi/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package com.dbc.walletapi.exceptions;
+
+public class RegraDeNegocioException extends Exception {
+    public RegraDeNegocioException(String mensagem){
+        super(mensagem);
+    }
+}
--- src/main/java/com/dbc/walletapi/service/GerenteService.java
+++ src/main/java/com/dbc/walletapi/service/GerenteService.java
@@ -5,7 +5,9 @@ import com.dbc.walletapi.dto.GerenteDTO;
 import com.dbc.walletapi.dto.UsuarioCreateDTO;
 import com.dbc.walletapi.dto.UsuarioDTO;
 import com.dbc.walletapi.entity.GerenteEntity;
+import com.dbc.walletapi.entity.RegraEntity;
 import com.dbc.walletapi.entity.UsuarioEntity;
+import com.dbc.walletapi.exceptions.RegraDeNegocioException;
 import com.dbc.walletapi.repository.GerenteRepository;
 import com.dbc.walletapi.repository.RegraRepository;
 import com.dbc.walletapi.repository.UsuarioRepository;
@@ -23,13 +25,14 @@ public class GerenteService {
     private final GerenteRepository gerenteRepository;
     private final RegraRepository regraRepository;
 
-    public GerenteDTO create(GerenteCreateDTO gerenteCreateDTO) {
+    public GerenteDTO create(GerenteCreateDTO gerenteCreateDTO) throws RegraDeNegocioException {
 
 
         UsuarioCreateDTO usuarioCreateDTO = gerenteCreateDTO.getUsuario();
         UsuarioEntity usuarioNovo = objectMapper.convertValue(usuarioCreateDTO, UsuarioEntity.class);
 
-        usuarioNovo.setRegraEntity(regraRepository.findById(usuarioCreateDTO.getRegra()).orElse(null));
+
+        usuarioNovo.setRegraEntity(regraRepository.findById(usuarioCreateDTO.getRegra()).orElseThrow(() -> new RegraDeNegocioException("Regra não encontrada!")));
         UsuarioEntity user = usuarioRepository.save(usuarioNovo);
 
 
