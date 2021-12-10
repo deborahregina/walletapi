cbca6b71cead3920b6b728cafc184e87a35f1b25
Leo-kardo <leonardo_c_g@hotmail.com>
2021-12-10 10:53:05 -0300

    Serviço entity e Serviço DTO criados

--- src/main/java/com/dbc/walletapi/dto/UsuarioDTO.java
+++ src/main/java/com/dbc/walletapi/dto/UsuarioDTO.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UsuarioDTO {
+    @NotNull
     private Integer idUsuario;
+    @NotNull
     private String usuario;
     private List<Integer> grupos;
 }
