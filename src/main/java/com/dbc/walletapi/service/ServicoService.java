86f16b1abd5a0c5d4a92068fdc92136707e16a06
deborah <deborahregs@gmail.com>
2021-12-21 16:13:17 -0300

    alteracao valor grafico

--- src/main/java/com/dbc/walletapi/service/ServicoService.java
+++ src/main/java/com/dbc/walletapi/service/ServicoService.java
@@ -167,16 +167,17 @@ public class ServicoService<ServicosDTO> {
 
         BigDecimal somaTotal = BigDecimal.ZERO;
         for(ServicoEntity servico :  servicoEntities) {
+            if(servico.getPeriocidade() == TipoPeriodicidade.MENSAL) {
+                somaTotal = somaTotal.add(servico.getValor());
+            }
             if (servico.getPeriocidade() == TipoPeriodicidade.TRIMESTRAL) {
-                somaTotal = somaTotal.add(servico.getValor().divide(BigDecimal.valueOf(3),2, RoundingMode.HALF_UP));
+                somaTotal = somaTotal.add(servico.getValor().divide(new BigDecimal("3"),2, RoundingMode.FLOOR));
             }
             if(servico.getPeriocidade() == TipoPeriodicidade.SEMESTRAL) {
-                somaTotal = somaTotal.add(servico.getValor().divide(BigDecimal.valueOf(6),2, RoundingMode.HALF_UP));
+                somaTotal = somaTotal.add(servico.getValor().divide(new BigDecimal("6"),2, RoundingMode.FLOOR));
             }
             if(servico.getPeriocidade() == TipoPeriodicidade.ANUAL) {
-                somaTotal = somaTotal.add(servico.getValor().divide(BigDecimal.valueOf(12),2, RoundingMode.HALF_UP));
-            } if(servico.getPeriocidade() == TipoPeriodicidade.MENSAL) {
-                somaTotal = somaTotal.add(servico.getValor());
+                somaTotal = somaTotal.add(servico.getValor().divide(new BigDecimal("12"),2, RoundingMode.FLOOR));
             }
         }
         return somaTotal;
